33e9c04565b20da451a3c0ffdeedf965328d7eb5
Dima <dsay@ciklum.com>
2014-05-16 13:57:09 +0300

    MVP

---
+++ MVP/MVP/DPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DPAppDelegate.h
+//  MVP
+//
+//  Created by Dima on 5/16/14.
+//  Copyright (c) 2014 Dima Sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
