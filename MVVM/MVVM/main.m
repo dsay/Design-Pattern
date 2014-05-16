9f98a64907d2914631ad28ec474010452bd1691b
Dima <dsay@ciklum.com>
2014-05-16 14:02:02 +0300

    mvvm

---
+++ MVVM/MVVM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MVVM
+//
+//  Created by Dima on 5/16/14.
+//  Copyright (c) 2014 Dima Sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
+    }
+}
